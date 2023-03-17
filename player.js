596a5084f00e61b3da98d6a7c4eb104dda6c2ac3
Mazen Al Swar <43959520+mazenswar@users.noreply.github.com>
2023-03-17 11:21:41 -0400

    state

--- player.js
+++ player.js
@@ -1,3 +1,5 @@
+import { Sitting, Running, Jumping, Falling } from "./playerStates.js";
+
 export class Player {
 	constructor(game) {
 		this.game = game;
@@ -10,13 +12,23 @@ export class Player {
 		this.maxSpeed = 10;
 		this.vy = 0;
 		this.weight = 1;
+		this.frameX = 0;
+		this.frameY = 0;
+		this.states = [
+			new Sitting(this),
+			new Running(this),
+			new Jumping(this),
+			new Falling(this),
+		];
+		this.currentState = this.states[0];
+		this.currentState.enter();
 	}
 
 	draw(context) {
 		context.drawImage(
 			this.image,
-			0,
-			0,
+			this.frameX * this.width,
+			this.frameY * this.height,
 			this.width,
 			this.height,
 			this.x,
@@ -26,6 +38,7 @@ export class Player {
 		);
 	}
 	update(input) {
+		this.currentState.handleInput(input);
 		// horizontal movement
 		this.x += this.speed;
 		if (input.includes("ArrowRight")) {
@@ -41,18 +54,22 @@ export class Player {
 			this.x = this.game.width - this.width;
 		}
 		// vertical movement
-		if (input.includes("ArrowUp") && this.onGround()) {
-			this.vy -= 20;
-		}
+
 		this.y += this.vy;
 		if (!this.onGround()) {
 			this.vy += this.weight;
 		} else {
 			this.vy = 0;
 		}
+		// sprite animation
 	}
 
 	onGround() {
 		return this.y >= this.game.height - this.height;
+	}
+
+	setState(state) {
+		this.currentState = this.states[state];
+		this.currentState.enter();
 	}
 }
--- playerStates.js
+++ playerStates.js
@@ -1,4 +1,4 @@
-states = {
+const states = {
 	SITTING: 0,
 	RUNNNING: 1,
 	JUMPING: 2,
@@ -11,42 +11,71 @@ class State {
 	}
 }
 
-class Sitting {
+export class Sitting extends State {
 	constructor(player) {
 		super("SITTING");
 		this.player = player;
 	}
 
-	enter() {}
-	handleInput(input) {}
+	enter() {
+		this.player.frameY = 5;
+	}
+	handleInput(input) {
+		if (input.includes("ArrowLeft") || input.includes("ArrowRight")) {
+			this.player.setState(states.RUNNNING);
+		}
+	}
 }
-
-class Running {
+///////////////////////////////////////////////
+export class Running extends State {
 	constructor(player) {
 		super("RUNNING");
 		this.player = player;
 	}
 
-	enter() {}
-	handleInput(input) {}
+	enter() {
+		this.player.frameY = 3;
+	}
+	handleInput(input) {
+		if (input.includes("ArrowDown")) {
+			this.player.setState(states.SITTING);
+		} else if (input.includes("ArrowUp")) {
+			this.player.setState(states.JUMPING);
+		}
+	}
 }
-
-class Jumping {
+///////////////////////////////////////////////
+export class Jumping extends State {
 	constructor(player) {
 		super("JUMPING");
 		this.player = player;
 	}
 
-	enter() {}
-	handleInput(input) {}
+	enter() {
+		this.player.frameY = 1;
+		if (this.player.onGround()) {
+			this.player.vy -= 30;
+		}
+	}
+	handleInput(input) {
+		if (this.player.vy > this.player.weight) {
+			this.player.setState(states.FALLING);
+		}
+	}
 }
-
-class Falling {
+///////////////////////////////////////////////
+export class Falling extends State {
 	constructor(player) {
 		super("FALLING");
 		this.player = player;
 	}
 
-	enter() {}
-	handleInput(input) {}
+	enter() {
+		this.player.frameY = 2;
+	}
+	handleInput(input) {
+		if (this.player.onGround()) {
+			this.player.setState(states.RUNNNING);
+		}
+	}
 }
